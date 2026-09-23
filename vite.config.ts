9261ce47fb60052678b7b7991767de5085244d63
sajidshoukat3282 <shaheen.sajid@gmail.com>
2026-09-24 03:53:05 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
-export default defineConfig({ plugins:[react()], resolve:{ alias:{ '@':path.resolve(__dirname,'src') } }, build:{ target:'es2020' } });
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
